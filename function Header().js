ba018bf8790e76bb37733d57c0f6c9f83b4ebfcf
Suffa Amjad <suffaamjad4@gmail.com>
2026-03-10 18:24:38 +0500

    version3

---
+++ function Header().js
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header style={{ backgroundColor: "#333", color: "white", padding: "15px", textAlign: "center" }}>
+      <h1>My Website Header</h1>
+    </header>
+  );
+}
+
+export default Header;
